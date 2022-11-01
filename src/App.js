166e02249ac27121c0cc114f3f437ed936a3812c
b0p1 <sofiarojo09@gmail.com>
2022-11-01 14:51:53 -0400

    add fonts and folder structure. remove uneccessary files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import "./styles/global.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeFeedPage />} />
+          {/* <Route path="/users/:id" element={<ProfilePage />} />
+          <Route path="/upload" element={<AddPostPage />} />
+          <Route path="*" element={<NotFoundPage/>} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
