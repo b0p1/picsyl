79c1ffb30d7712ea0ee09f7d8fc140795d9e8fbb
b0p1 <sofiarojo09@gmail.com>
2022-11-05 19:32:27 -0400

    add home button and link it to home page

--- src/components/MobileFooter/MobileFooter.js
+++ src/components/MobileFooter/MobileFooter.js
@@ -1,19 +1,32 @@
 import "./mobile-footer.scss";
+import homeIcon from "../../assets/icons/home.svg";
 import addPostIcon from "../../assets/icons/camera-plus.svg";
 import profileIcon from "../../assets/icons/user.svg";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function MobileFooter(props) {
   return (
     <div className="mobile-footer">
       <div className="mobile-footer__icon-container">
-        <Link to="/upload">
-        <img className="mobile-footer__icon mobile-footer__icon-mid" src={addPostIcon} />
-
+        <Link to="/">
+          <img
+            className="mobile-footer__icon mobile-footer__icon-left"
+            src={homeIcon}
+          />
         </Link>
-        <img className="mobile-footer__icon mobile-footer__icon-right" src={profileIcon} />
+        <Link to="/upload">
+          <img
+            className="mobile-footer__icon mobile-footer__icon-mid"
+            src={addPostIcon}
+          />
+        </Link>
+        <Link to="/users/5">
+          <img
+            className="mobile-footer__icon mobile-footer__icon-right"
+            src={profileIcon}
+          />
+        </Link>
       </div>
-      
     </div>
   );
 }
